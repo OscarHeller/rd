534200ca6833d933cdd464b366f98e41a756281c
Oscar Heller <oheller@gmail.com>
2016-04-05 15:23:01 -0400

    Revmoed Commands and Score commands.

--- game/lib/interpreters/status.py
+++ game/lib/interpreters/status.py
@@ -18,34 +18,4 @@ minPosition			(Position.standing)	: what is the minimum position as defined in c
 """
 
 
-class Score(Command):
-	def __init__(self, game):
-		super(Score, self).__init__(game, 'score')
-		self.minPosition = Position.sleeping
-		self.useInCombat = True
-
-	def execute(self, args, config):
-		sender = config['sender']
-		buf = 'Score:'
-		buf += '\n\rDatabase ID: {id}'.format(id=sender.id)
-		for s in sender.stats:
-			if sender.stats[s]:
-				buf += '\n\r{statName}: {stat}'.format(statName=s, stat=sender.stats[s])
-		sender.sendToClient(buf)
-
-
-class Commands(Command):
-	def __init__(self, game):
-		super(Commands, self).__init__(game, 'commands')
-		self.minPosition = Position.sleeping
-		self.useInCombat = True
-
-	def execute(self, args, config):
-		sender = config['sender']
-		buf = 'Commands:\n\r'
-		for commandObject in sender.commandInterpreter.commandInterpreters:
-			buf += commandObject.__name__ + '\n\r'
-		sender.sendToClient(buf)
-
-
-commandList = [Score, Commands]
+commandList = []
